e1ad2a38df3ba22fac4f9b5aebd7e4b348dfcb5b
Nate <etlf74@yandex.ru>
2022-10-26 22:28:48 +0500

    added Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,56 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов
+
+void FillMatrix(int[,] matr, int min, int max)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();    
+    }
+}
+
+Console.WriteLine("Введите размер массива: ");
+
+Console.Write("Количество строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Количество столбцов: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] matrix = new int[m, n];
+
+FillMatrix(matrix, 1, 20);
+PrintMatrix(matrix);
+
+Console.WriteLine();
+
+int[] array = new int[n];
+
+for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int sumColumn = 0;
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sumColumn += matrix[i,j];
+        }
+        array[j] = sumColumn;
+    }  
+
+Console.WriteLine(); 
+Console.Write($"Среднее арифметическое по каждому столбцу: ");
+Console.WriteLine();
+for(int j = 0; j < n; j++) Console.Write($"{array[j] / m} ");
